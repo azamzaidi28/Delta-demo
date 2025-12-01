54686c21adab47e2ae7026b0c4d1c51196a8d736
azamzaidi28 <azamzaidi28@gmail.com>
2025-12-01 14:50:30 +0530

    add some code

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let arr = [1,2,3,4,5,7,8];
+let copy_arr = [...arr];
+copy_arr.push(9);
+console.log(arr)
+console.log(copy_arr);
+
+let str = ["ApnaCollege"];
+let newStr = [...str]
+
+console.log(newStr)
+console.log(..."ApnaCollege");
